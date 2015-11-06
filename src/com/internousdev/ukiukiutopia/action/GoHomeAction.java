d795b1c2d7981887b320a5e3fa5ba060be1d0cb0
Naohiro Minami <hdeazpya@gmail.com>
2015-11-06 17:44:31 +0900

    action

---
+++ src/com/internousdev/ukiukiutopia/action/GoHomeAction.java
@@ -0,0 +1,12 @@
+package com.internousdev.ukiukiutopia.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GoHomeAction extends ActionSupport{
+
+	private static final long serialVersionUID = 1L;
+
+	public String execute(){
+		return SUCCESS;
+	}
+}
---
+++ src/com/internousdev/ukiukiutopia/action/GoTicketBuyAction.java
@@ -0,0 +1,12 @@
+package com.internousdev.ukiukiutopia.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GoTicketBuyAction extends ActionSupport{
+
+	private static final long serialVersionUID = 1L;
+
+	public String execute(){
+		return SUCCESS;
+	}
+}
