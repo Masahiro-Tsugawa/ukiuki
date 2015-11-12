ceac02c42f2c69050bc3de47953ef1b59029bef5
Katsuyuki Kato <wk_bad.9ball@docomo.ne.jp>
2015-11-12 17:42:11 +0900

    か

--- src/com/internousdev/ukiukiutopia/dao/PersonalInformationDAO.java
+++ src/com/internousdev/ukiukiutopia/dao/PersonalInformationDAO.java
@@ -10,60 +10,68 @@ import java.util.List;
 import com.internousdev.ukiukiutopia.dto.PersonalInformationDTO;
 import com.internousdev.ukiukiutopia.util.DBConnector;
 
-
-
 public class PersonalInformationDAO {
 
 	Connection con;
 	boolean action;
 
+	public List<PersonalInformationDTO> PersonalList = new ArrayList<PersonalInformationDTO>();
 
-	public List<PersonalInformationDTO>PersonalList  = new ArrayList<PersonalInformationDTO>();
-
-//検索するメソッド
-	public boolean select()throws Exception{
+	// 検索するメソッド
+	public boolean select(String emailaddress) throws Exception {
 		System.out.println("select - メソッド実行");
-		action=false;
+		System.out.println("EmailAddr:" + emailaddress + "を検索");
+		action = false;
 		con = DBConnector.getConnection();
 
-		try{
-		String sql = "select * from user";
+		try {
+			String sql = "select * from user where email = ?";
 
-		PreparedStatement ps2;
-		ps2 = con.prepareStatement(sql);
+			PreparedStatement ps2;
+			ps2 = con.prepareStatement(sql);
 
-		System.out.println("select - ps2 - "+ ps2);
+			System.out.println("select - ps2 - " + ps2);
+			ps2.setString(1, emailaddress);
+			System.out.println("select - sql実行");
+			ResultSet rs = ps2.executeQuery();
+			System.out.println(rs);
+			while (rs.next()) {
+				action = true;
 
-		ResultSet rs = ps2.executeQuery();
-		System.out.println("select - sql実行");
+				PersonalInformationDTO dto = new PersonalInformationDTO();
+				System.out.println(rs.getInt(1));
+				System.out.println(rs.getString(2));
+				System.out.println(rs.getString(3));
+				System.out.println(rs.getString(4));
+				System.out.println(rs.getString(5));
+				System.out.println(rs.getString(6));
 
-		while(rs.next()){
-			action = true;
+				dto.setId(rs.getInt(1));
+				dto.setName(rs.getString(2));
+				dto.setTel_num(rs.getString(3));
+				dto.setEmail(rs.getString(4));
+				dto.setPostal(rs.getString(5));
+				dto.setAddress(rs.getString(6));
 
-			PersonalInformationDTO dto =new PersonalInformationDTO();
-			dto.setId(rs.getInt(1));
-			dto.setName(rs.getString(2));
-			dto.setTel_num(rs.getString(3));
-			dto.setEmail(rs.getString(4));
-			dto.setPostal(rs.getString(5));
-			dto.setAddress(rs.getString(6));
+				PersonalList.add(dto);
+				System.out.println("select - itemList - OK");
+			} // while
 
-			PersonalList.add(dto);
-			System.out.println("select - itemList - OK");
-		}//while
-
-	}catch(Exception e){
-		e.printStackTrace();
-	}finally{
-		con.close();
-	}//finally
-		System.out.println("select - return - "+ action);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			con.close();
+		} // finally
+		System.out.println("select - return - " + action);
 		return action;
 	}
 
-	public Collection<? extends PersonalInformationDTO> getPersonalList() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public List<PersonalInformationDTO> getPersonalList() {
+		return PersonalList;
 	}
 
-	}//select
+	public void setPersonalList(List<PersonalInformationDTO> personalList) {
+		PersonalList = personalList;
+	}
+
+}
