cfe3e5b914c845e23998adcf88a9fcf086ab717b
tsuga_scrum@hotmail.co.jp <'tsuga_scrum@hotmail.co.jp'>
2015-11-18 13:56:59 +0900

    Tsugawa

--- src/com/internousdev/ukiukiutopia/dao/CreateUserDAO.java
+++ src/com/internousdev/ukiukiutopia/dao/CreateUserDAO.java
@@ -7,9 +7,19 @@ import java.sql.SQLException;
 
 import com.internousdev.ukiukiutopia.util.DBConnector;
 
+/**
+ * 新規登録メールアドレスが既にDBに登録されてないか確かめる為のクラス
+ * @author M.Tsugawa
+ * @version 1.1
+ * @since 1.0
+ */
 public class CreateUserDAO {
 	private String email;
-
+    /**
+     * 新規登録メールアドレスが既にDBに登録されてないか確かめるメソッド
+     * @param userEmail 登録されているメールアドレス
+     * @return 既存アドレスがあるかの有無
+     */
 	public String select(String userEmail) {
 
 		Connection conn = null;
@@ -23,7 +33,7 @@ public class CreateUserDAO {
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
 				ret = "error";
-				email = rs.getString("email");
+				setEmail(rs.getString("email"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -38,13 +48,19 @@ public class CreateUserDAO {
 		}
 		return ret;
 	}
-
-	public String getAdmin_name() {
+	
+	/**
+	 * 新規登録メールアドレスを取得するメソッド
+	 * @return 新規登録メールアドレス
+	 */
+	public String getEmail() {
 		return email;
 	}
-
-	public void setAdmin_name(String email) {
+	/**
+	 * 新規登録メールアドレスを設定するメソッド
+	 * @param email 新規登録メールアドレス
+	 */
+	public void setEmail(String email) {
 		this.email = email;
 	}
-
 }
