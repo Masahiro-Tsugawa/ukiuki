5713508cdbd160462a23da09189fb84d22f51831
masui. <inkaundeal@gmail.com>
2015-11-19 19:42:19 +0900

    コミット　struts書き換え　増井

--- src/com/internousdev/ukiukiutopia/util/GoogleOauth.java
+++ src/com/internousdev/ukiukiutopia/util/GoogleOauth.java
@@ -56,7 +56,7 @@ public class GoogleOauth extends ActionSupport {
 		try{
 			String apiKey = "42056507966-0pf52q6nrt5l30eqijjm30h1jm8l19vh.apps.googleusercontent.com";
 			String apiSecret = "IcSSbPEavOsH8H2WcACPpNeB";
-			String callbackUrl = "http://localhost:8080/ukiukiutopia/my_page.jsp";
+			String callbackUrl = "http://localhost:8080/ukiukiutopia/login-google-action";
 
 			OAuthService service = new ServiceBuilder()
 			.provider(GoogleApi.class)
